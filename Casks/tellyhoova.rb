2948c35a955a476230cab116fd8d043fd56b8d38
Mat Chavez <1787040+matchavez@users.noreply.github.com>
2026-05-29 08:59:07 +1200

    Add Gatekeeper caveat for unsigned app

--- Casks/tellyhoova.rb
+++ Casks/tellyhoova.rb
@@ -11,6 +11,17 @@ cask "tellyhoova" do
 
   app "Tellyhoova.app"
 
+  caveats <<~EOS
+    Tellyhoova is not code-signed. If macOS says the app is "damaged",
+    run the following command after installation:
+
+      xattr -dr com.apple.quarantine /Applications/Tellyhoova.app
+
+    Alternatively, install with quarantine disabled:
+
+      brew install --cask --no-quarantine matchavez/tellyhoova/tellyhoova
+  EOS
+
   zap trash: [
     "~/Library/Application Support/Tellyhoova",
     "~/Library/Preferences/com.matchavez.tellyhoova.plist",
